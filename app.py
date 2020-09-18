cdfe9c525c6abe8fe91c085c0069b709ef4cf810
AREEG94FAHAD <fahedareeg@gmail.com>
2020-09-17 22:12:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,43 @@
 import os
 import json
 import requests
-
 from flask import Flask, render_template, request
-from flask_assets import Bundle,Environment
-
+from flask_assets import Bundle, Environment
 app = Flask(__name__)
-
 
 
 @app.route("/")
 def index():
     r = requests.get('https://pomber.github.io/covid19/timeseries.json')
-    f=json.loads(r.text)['Iraq']
-    deaths=[]
-    date=[]
-    confirmed=[]
-    recovered=[]
+    IraqInfo = json.loads(r.text)['Iraq']
+    deaths = []
+    date = []
+    confirmed = []
+    recovered = []
     legend = 'Monthly Data'
-    for i in f:
+    for i in IraqInfo:
         date.append(i['date'])
-    for i in f:
+    for i in IraqInfo:
         deaths.append(i['deaths'])
-    for i in f:
+    for i in IraqInfo:
         confirmed.append(i['confirmed'])
-    for i in f:
-        if i['recovered']!=None:
+    for i in IraqInfo:
+        if i['recovered'] is not None:
             recovered.append(i['recovered'])
     print(recovered)
-    maxdeaths=max(deaths)
-    maxconfirmed=max(confirmed)
-    maxrecovered=max(recovered)
-    active=max(confirmed)-max(recovered)-max(deaths)
+    maxdeaths = max(deaths)
+    maxconfirmed = max(confirmed)
+    maxrecovered = max(recovered)
+    active = max(confirmed) - max(recovered) - max(deaths)
 
-    return render_template("index.html", f=f,deaths=deaths,date=date,confirmed=confirmed,recovered=recovered,maxdeaths=maxdeaths,maxconfirmed=maxconfirmed,maxrecovered=maxrecovered, active=active)
+    return render_template(
+        "index.html",
+        IraqInfo=IraqInfo,
+        deaths=deaths,
+        date=date,
+        confirmed=confirmed,
+        recovered=recovered,
+        maxdeaths=maxdeaths,
+        maxconfirmed=maxconfirmed,
+        maxrecovered=maxrecovered,
+        active=active)
